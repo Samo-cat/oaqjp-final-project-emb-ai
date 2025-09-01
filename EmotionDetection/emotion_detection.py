88521a19d8199242d96b63f2284e855623bf46f2
Samo-cat <samosational@gmail.com>
2025-09-01 19:35:44 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = headers)
+    
+    formatted_response = json.loads(response.text)
+
+    emotion_predictions = formatted_response.get('emotionPredictions')
+
+    if emotion_predictions:
+            emotions = emotion_predictions[0].get('emotion')
+            anger_score = emotions.get('anger', 0)
+            disgust_score = emotions.get('disgust', 0)
+            fear_score = emotions.get('fear', 0)
+            joy_score = emotions.get('joy', 0)
+            sadness_score = emotions.get('sadness', 0)
+
+            dominant_emotion = max(emotions, key=emotions.get)
+
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    return result
