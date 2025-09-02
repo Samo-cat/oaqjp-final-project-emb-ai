ab0b5e3f607cfb71e9ae05b10a50c09c62fcc34e
Samo-cat <samosational@gmail.com>
2025-09-02 00:09:57 -0400

    Initial Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,20 +7,29 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers = headers)
+    status_code = response.status_code
     
     formatted_response = json.loads(response.text)
 
     emotion_predictions = formatted_response.get('emotionPredictions')
 
+    if status_code == 400:
+        dominant_emotion = { 'anger': None,
+                             'disgust': None,
+                             'fear': None,
+                             'joy': None,
+                             'sadness': None,
+                             'dominant_emotion': None }
+                             
     if emotion_predictions:
-            emotions = emotion_predictions[0].get('emotion')
-            anger_score = emotions.get('anger', 0)
-            disgust_score = emotions.get('disgust', 0)
-            fear_score = emotions.get('fear', 0)
-            joy_score = emotions.get('joy', 0)
-            sadness_score = emotions.get('sadness', 0)
+        emotions = emotion_predictions[0].get('emotion')
+        anger_score = emotions.get('anger', 0)
+        disgust_score = emotions.get('disgust', 0)
+        fear_score = emotions.get('fear', 0)
+        joy_score = emotions.get('joy', 0)
+        sadness_score = emotions.get('sadness', 0)
 
-            dominant_emotion = max(emotions, key=emotions.get)
+        dominant_emotion = max(emotions, key=emotions.get)
 
     result = {
         'anger': anger_score,
